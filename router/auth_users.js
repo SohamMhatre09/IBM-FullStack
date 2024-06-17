24f9132c9a0c97f4b56e04fa9461d334326b8c10
SohamMhatre09 <ichbinsoham@gmail.com>
2024-06-17 23:02:53 +0530

    first commit

---
+++ router/auth_users.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+let books = require("./booksdb.js");
+const regd_users = express.Router();
+const SECRET_KEY = 'your_secret_key';
+let users = [{ username: "username", password: "password" }];
+
+const isValid = (username) => {
+  //returns boolean
+  //write code to check is the username is valid
+};
+
+const authenticatedUser = (username, password) => {
+  for (const user of users) {
+    if (user.username === username) {
+      if (user.password === password) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+//only registered users can login
+regd_users.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  if (authenticatedUser(username,password))
+    {
+      const token = jwt.sign({ username }, SECRET_KEY, { expiresIn: "1h" });
+      return res.status(200).json({ message: "Logged In Succesfully",token});
+    }
+  return res.status(400).json({ message: "Username or Password doesnt match!" });
+});
+
+// Add a book review
+// Add a book review
+regd_users.put("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+  const review = req.body.review;
+  const username = req.user.username; // Assuming the username is stored in the session
+  if (!books[isbn]) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+  if (!books[isbn].reviews) {
+    books[isbn].reviews = {};
+  }
+  books[isbn].reviews[username] = review;
+  return res.status(200).json({ message: "Review added/modified successfully" });
+});
+
+// Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+  const username = req.user.username; // Assuming the username is stored in the session
+
+  if (!books[isbn]) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+
+  if (!books[isbn].reviews || !books[isbn].reviews[username]) {
+    return res.status(404).json({ message: "Review not found" });
+  }
+
+  delete books[isbn].reviews[username];
+
+  return res.status(200).json({ message: "Review deleted successfully" });
+});
+
+module.exports.authenticated = regd_users;
+module.exports.isValid = isValid;
+module.exports.users = users;
---
+++ router/general.js
@@ -0,0 +1,88 @@
+const express = require("express");
+let books = require("./booksdb.js");
+let isValid = require("./auth_users.js").isValid;
+let users = require("./auth_users.js").users;
+const public_users = express.Router();
+const SECRET_KEY = 'your_secret_key';
+const jwt = require('jsonwebtoken');
+public_users.post("/register", (req, res) => {
+  const { username, password } = req.body;
+  for (const user of users) {
+    if (user.username === username) {
+      return res.status(400).json({ message: "Username already exists" });
+    }
+  }
+  users.push({ username, password });
+  const token = jwt.sign({ username }, SECRET_KEY, { expiresIn: "1h" });
+  return res
+    .status(200)
+    .json({ message: "User successfully registered", token });
+});
+
+// Get the book list available in the shop..
+public_users.get("/", function (req, res) {
+  return res.status(300).json(books);
+});
+
+// Get book details based on ISBN
+public_users.get("/isbn/:isbn", function (req, res) {
+  const book_isbn = books[req.params.isbn];
+
+  if (book) {
+    return res.status(200).json(book);
+  } else {
+    return res.status(404).json({ message: "Book not found" });
+  }
+});
+
+// Get book details based on author
+public_users.get("/author/:author", function (req, res) {
+  const name = req.params.author.toLowerCase().trim();
+  const authorBooks = [];
+
+  for (const isbn in books) {
+    if (
+      books[isbn].author &&
+      books[isbn].author.toLowerCase().trim() === name
+    ) {
+      authorBooks.push(books[isbn]);
+    }
+  }
+
+  if (authorBooks.length > 0) {
+    return res.status(200).json(authorBooks);
+  } else {
+    return res.status(404).json({ message: "Book not found" });
+  }
+});
+
+// Get all books based on title
+public_users.get("/title/:title", function (req, res) {
+  const title = req.params.title.toLowerCase().trim();
+  const authorBooks = [];
+
+  for (const isbn in books) {
+    if (books[isbn].title && books[isbn].title.toLowerCase().trim() === title) {
+      authorBooks.push(books[isbn]);
+    }
+  }
+
+  if (authorBooks.length > 0) {
+    return res.status(200).json(authorBooks);
+  } else {
+    return res.status(404).json({ message: "Book not found" });
+  }
+});
+
+//  Get book review
+public_users.get("/review/:isbn", function (req, res) {
+  const book = books[req.params.isbn];
+
+  if (book) {
+    return res.status(200).json(book.reviews);
+  } else {
+    return res.status(404).json({ message: "Book not found" });
+  }
+});
+
+module.exports.general = public_users;
